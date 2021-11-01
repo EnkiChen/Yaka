e89438e829365c39fbb58157e3e8da2d5f7504c7
EnkiChen <xc120313778@163.com>
2021-11-01 09:47:38 +0800

    feat: 添加多路播放的 UI 类

---
+++ Yaka/ViewCtrl/MultiPlayViewCtrl.h
@@ -0,0 +1,19 @@
+//
+//  MultiPlayViewCtrl.h
+//  Yaka
+//
+//  Created by Enki on 2021/10/30.
+//  Copyright © 2021 Enki. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MultiPlayViewCtrl : NSViewController
+
+
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Yaka/ViewCtrl/MultiPlayViewCtrl.m
@@ -0,0 +1,24 @@
+//
+//  MultiPlayViewCtrl.m
+//  Yaka
+//
+//  Created by Enki on 2021/10/30.
+//  Copyright © 2021 Enki. All rights reserved.
+//
+
+#import "MultiPlayViewCtrl.h"
+
+@interface MultiPlayViewCtrl ()
+
+@property (weak) IBOutlet NSStackView *verticalStackView;
+
+@end
+
+@implementation MultiPlayViewCtrl
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end
